01353b9bb5c6286d9714d02fe89b59073e330a1e
Emily Chiew <Emily@192-168-1-105.tpgi.com.au>
2022-03-17 23:36:38 +1100

    update

--- PHASE_1/test_main.py
+++ PHASE_1/test_main.py
@@ -5,23 +5,28 @@ from src.main import app
 
 client = TestClient(app)
 
-## articles
-def test_get_all_articles():
-    response = client.get("/articles")
-    assert response.status_code == 200
-
+## general get endpoints
 def test_get():
     response = client.get("/")
     assert response.status_code == 200
 
+def test_get_all_articles():
+    response = client.get("/articles")
+    assert response.status_code == 200
 
-## reports
+def test_get_all_reports():
+    response = client.get("/articles/reports")
+    assert response.status_code == 200
 
+## endpoint errors
+def test_wrong_endpont():
+    response = client.get("/reports")
+    assert response.status_code == 400
 
+## queries
+def test_get_article_by_disease(): 
+    assert 5 == 5
 
-## diseases
-
-
-## locations
-
-## syndromes
+## query errors
+def test_date_after_today():
+    assert 5 == 5
