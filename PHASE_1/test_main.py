24d4ca44a3b4213c9418930232fe4893674d4a20
Emily Chiew <Emily@192-168-1-105.tpgi.com.au>
2022-03-18 12:03:00 +1100

    commit

--- PHASE_1/test_main.py
+++ PHASE_1/test_main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
-import json
 
 from src.main import app
 
@@ -27,18 +26,19 @@ def test_get_all_reports():
 
 ## get article by id
 
-# def test_article_by_id():
-#     response = client.get("/articles/623327582c76fb9a3c327706")
-#     assert response.status_code == 200
-#     data = json.loads(response.content)
-#     assert data["_id"] == "623327582c76fb9a3c327706"
+def test_article_by_id():
+    response = client.get("/articles/623327582c76fb9a3c327706")
+    assert response.status_code == 200
+    data = response.json()
+    assert str(data["d"]["_id"]) == "623327582c76fb9a3c327706"
 
 def test_article_by_nonexistant_id():
     response = client.get("/articles/0")
     assert response.status_code == 400
 
-# def test_get_reports_by_article_with_id():
-
+def test_get_reports_by_article_with_id():
+    response = client.get("/articles/623327582c76fb9a3c327706/reports")
+    assert response.status_code == 200
 
 ## get article by disease
 def test_get_article_by_disease(): 
