76f6ec50c9724baf65a026acb8be3a26afa7ddbb
Emily Chiew <Emily@192-168-1-105.tpgi.com.au>
2022-03-18 13:25:50 +1100

    update

--- PHASE_1/test_main.py
+++ PHASE_1/test_main.py
@@ -1,4 +1,3 @@
-from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
 from src.main import app
@@ -18,39 +17,40 @@ def test_get_all_reports():
     response = client.get("/reports")
     assert response.status_code == 200
 
-# def test_nonexistant_endpont():
-#     response = client.get("/articles/reports")
+def test_nonexistant_endpont():
+    response = client.get("/articles/reports")
+    assert response.status_code == 400
+
+
+
+# ## get article by id
+
+# def test_article_by_id():
+#     drop()
+#     response = client.get("/articles/")
+#     assert response.status_code == 200
+#     data = response.json()
+#     assert str(data["d"]["_id"]) == {id}
+
+# def test_article_by_nonexistant_id():
+#     response = client.get("/articles/0")
 #     assert response.status_code == 400
 
+# def test_get_reports_by_article_with_id():
+#     response = client.get("/articles/6233df32c6f4ddf1537ad156/reports")
+#     assert response.status_code == 200
 
+# ## get article by disease
+# def test_get_article_by_disease(): 
+#     response = client.get("/articles?key_term=coronavirus")
+#     assert response.status_code == 200
 
-## get article by id
+# def test_query_nonexistant_disease():
+#     response = client.get("/articles?key_term=non_existant_disease")
+#     assert response.status_code == 200
+#     assert response.json() == []
 
-def test_article_by_id():
-    response = client.get("/articles/623327582c76fb9a3c327706")
-    assert response.status_code == 200
-    data = response.json()
-    assert str(data["d"]["_id"]) == "623327582c76fb9a3c327706"
-
-def test_article_by_nonexistant_id():
-    response = client.get("/articles/0")
-    assert response.status_code == 400
-
-def test_get_reports_by_article_with_id():
-    response = client.get("/articles/623327582c76fb9a3c327706/reports")
-    assert response.status_code == 200
-
-## get article by disease
-def test_get_article_by_disease(): 
-    response = client.get("/articles?key_term=coronavirus")
-    assert response.status_code == 200
-
-def test_query_nonexistant_disease():
-    response = client.get("/articles?key_term=non_existant_disease")
-    assert response.status_code == 200
-    assert response.json() == []
-
-## query errors
-def test_date_after_today():
-    response = client.get("/articles?start_date=2")
-    assert response.status_code == 400
+# ## query errors
+# def test_date_after_today():
+#     response = client.get("/articles?start_date=2")
+#     assert response.status_code == 400
