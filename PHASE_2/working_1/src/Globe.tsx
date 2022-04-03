272e57a497c721115c9d6438046b791f8f9ba3d5
Rahul Tripathi <rahultripathi0401@gmail.com>
2022-04-03 17:33:48 +1000

    added working globe

---
+++ PHASE_2/working_1/src/Globe.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactGlobe from "react-globe.gl";
+import { renderToStaticMarkup } from "react-dom/server";
+
+const _renderPopup = (index: number) => {
+  return (
+    <div className="Popup">
+      <div className="Popup__title">Popup</div>
+      <div className="Popup__content">Item: {index}</div>
+    </div>
+  );
+};
+
+const N = 20;
+const arcsData = [...Array(N).keys()].map((_, index) => ({
+  startLat: (Math.random() - 0.5) * 180,
+  startLng: (Math.random() - 0.5) * 360,
+  endLat: (Math.random() - 0.5) * 180,
+  endLng: (Math.random() - 0.5) * 360,
+  name: renderToStaticMarkup(_renderPopup(index)),
+  color: [["red", "white", "blue", "green"][Math.round(Math.random() * 3)], ["red", "white", "blue", "green"][Math.round(Math.random() * 3)]],
+  endpoint: `https://google.com?q=${index}`
+}));
+
+function Globe() {
+  return (
+    <div className="Globe">
+      <ReactGlobe
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+        arcsData={arcsData}
+        arcColor={"color"}
+        arcStroke={1.2}
+        arcDashLength={() => Math.random()}
+        arcDashGap={() => Math.random()}
+        arcDashAnimateTime={() => 4000}
+
+      />
+    </div>
+  );
+}
+
+export default Globe;
---
+++ PHASE_2/working_1/src/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Globe from "./Globe";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <Switch>
+        <Route exact path="/" component={Globe} />
+      </Switch>
+    </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
