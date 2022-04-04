05e712e11bdf76c70dc08fa6c19db37e40f5940e
Rahul Tripathi <rahultripathi0401@gmail.com>
2022-04-04 21:17:42 +1000

    heatmap map working

--- PHASE_2/app/src/Globe.tsx
+++ PHASE_2/app/src/Globe.tsx
@@ -1,32 +1,49 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import ReactGlobe from "react-globe.gl";
-import data from "./data/pretty.json"
+import * as d3 from "d3";
+
 function Globe() {
-  const [places, setPlaces] = useState([]);
-  // useEffect(() => {
-  //   let test = JSON.parse(data)
-  //   setPlaces(test)
-  //   // setPlaces(JSON.parse(data))
-  // }, []);
+  const [countries, setCountries] = useState({ features: []});
+  const [hoverD, setHoverD] = useState();  
   useEffect(() => {
     // load data
-    fetch('pretty.json').then(res => res.json())
-      .then(({ features }) => setPlaces(features));
+    fetch('output_single_map.json')
+    .then(res => res.json())
+    .then(setCountries)
+    .catch((e) => console.error(e));
   }, []);
+
+  const colorScale = d3.scaleSequentialSqrt(d3.interpolateYlOrRd);
+
+  // GDP per capita (avoiding countries with small pop)
+  const getVal = (feat : any) => feat.properties.GDP_MD_EST / Math.max(1e5, feat.properties.POP_EST);
+
+  const maxVal = useMemo(
+    () => Math.max(...countries.features.map(getVal)),
+    [countries]
+  );
+  colorScale.domain([0, maxVal]);
+  
   return (
     <div className="Globe">
-    <ReactGlobe
+      <ReactGlobe
       globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
       backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
-      labelsData={places}
-      labelLat={(d : any) => d.properties.latitude}
-      labelLng={(d : any) => d.properties.longitude}
-      labelText={(d : any) => d.properties.name}
-      labelSize={(d : any) => Math.sqrt(d.properties.pop_max) * 4e-4}
-      labelDotRadius={(d : any) => Math.sqrt(d.properties.pop_max) * 4e-4}
-      labelColor={() => 'rgba(255, 165, 0, 0.75)'}
-      labelResolution={2}
-      />
+      // lineHoverPrecision={0}
+
+      // polygonsData={countries.features.filter((d : any) => d.properties.ISO_A2 !== 'AQ')}
+      polygonAltitude={d => d === hoverD ? 0.12 : 0.06}
+      polygonCapColor={d => d === hoverD ? 'steelblue' : colorScale(getVal(d))}
+      polygonSideColor={() => 'rgba(0, 100, 0, 0.15)'}
+      polygonStrokeColor={() => '#111'}
+      polygonLabel={({ properties: d } : any) => `
+        <b>${d.ADMIN} (${d.ISO_A2}):</b> <br />
+        GDP: <i>${d.GDP_MD_EST}</i> M$<br/>
+        Population: <i>${d.POP_EST}</i>
+      `}
+      // onPolygonHover={(setHoverD)}
+      polygonsTransitionDuration={300}
+    />
     </div>
   );
 }
