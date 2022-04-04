bfb8d1ab3915b235c13b34a375e8728cdea8c547
Rahul Tripathi <rahultripathi0401@gmail.com>
2022-04-04 19:56:35 +1000

    adding data

--- PHASE_2/app/src/Globe.tsx
+++ PHASE_2/app/src/Globe.tsx
@@ -1,39 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactGlobe from "react-globe.gl";
-import { renderToStaticMarkup } from "react-dom/server";
-
-const _renderPopup = (index: number) => {
-  return (
-    <div className="Popup">
-      <div className="Popup__title">Popup</div>
-      <div className="Popup__content">Item: {index}</div>
-    </div>
-  );
-};
-
-const N = 20;
-const arcsData = [...Array(N).keys()].map((_, index) => ({
-  startLat: (Math.random() - 0.5) * 180,
-  startLng: (Math.random() - 0.5) * 360,
-  endLat: (Math.random() - 0.5) * 180,
-  endLng: (Math.random() - 0.5) * 360,
-  name: renderToStaticMarkup(_renderPopup(index)),
-  color: [["red", "white", "blue", "green"][Math.round(Math.random() * 3)], ["red", "white", "blue", "green"][Math.round(Math.random() * 3)]],
-  endpoint: `https://google.com?q=${index}`
-}));
-
+// import data from "./data/map"
 function Globe() {
+  const [places, setPlaces] = useState([]);
+  useEffect(() => {
+    // load data
+    fetch('./data/map.geojson')
+      .then(res => res.json())
+      .then(({ features }) => {
+        setPlaces(features)
+        console.log(features)
+      })
+      .catch((e) => console.log(e));
+    }, []);
+
   return (
     <div className="Globe">
-      <ReactGlobe
-        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-        arcsData={arcsData}
-        arcColor={"color"}
-        arcStroke={1.2}
-        arcDashLength={() => Math.random()}
-        arcDashGap={() => Math.random()}
-        arcDashAnimateTime={() => 4000}
+    <ReactGlobe
+      globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+      backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
 
+      labelsData={places}
+      labelLat={(d : any) => d.properties.latitude}
+      labelLng={(d : any) => d.properties.longitude}
+      labelText={(d : any) => d.properties.name}
+      labelSize={(d : any) => Math.sqrt(d.properties.pop_max) * 4e-4}
+      labelDotRadius={(d : any) => Math.sqrt(d.properties.pop_max) * 4e-4}
+      labelColor={() => 'rgba(255, 165, 0, 0.75)'}
+      labelResolution={2}
       />
     </div>
   );
