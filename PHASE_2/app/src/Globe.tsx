3dced03be807d3981cd5e1995b83bf92e53d051c
Noa Challis <z5169766@ad.unsw.edu.au>
2022-04-07 14:37:32 +1000

    styled play/pause button for slider, added forntightly maps

--- PHASE_2/app/src/Globe.tsx
+++ PHASE_2/app/src/Globe.tsx
@@ -8,35 +8,63 @@ import Switch, { SwitchProps } from '@mui/material/Switch';
 import { styled, } from '@mui/material/styles';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import { FaPlay, FaPause } from 'react-icons/fa';
 
 const dates = [
   "2020-01-01_map.json",
+  "2020-01-15_map.json",
   "2020-02-01_map.json",
+  "2020-02-15_map.json",
   "2020-03-01_map.json",
+  "2020-03-15_map.json",
   "2020-04-01_map.json",
+  "2020-04-15_map.json",
   "2020-05-01_map.json",
+  "2020-05-15_map.json",
   "2020-06-01_map.json",
+  "2020-06-15_map.json",
   "2020-07-01_map.json",
+  "2020-07-15_map.json",
   "2020-08-01_map.json",
+  "2020-08-15_map.json",
   "2020-09-01_map.json",
+  "2020-09-15_map.json",
   "2020-10-01_map.json",
+  "2020-10-15_map.json",
   "2020-11-01_map.json",
+  "2020-11-15_map.json",
   "2020-12-01_map.json",
+  "2020-12-15_map.json",
   "2021-01-01_map.json",
+  "2021-01-15_map.json",
   "2021-02-01_map.json",
+  "2021-02-15_map.json",
   "2021-03-01_map.json",
+  "2021-03-15_map.json",
   "2021-04-01_map.json",
+  "2021-04-15_map.json",
   "2021-05-01_map.json",
+  "2021-05-15_map.json",
   "2021-06-01_map.json",
+  "2021-06-15_map.json",
   "2021-07-01_map.json",
+  "2021-07-15_map.json",
   "2021-08-01_map.json",
+  "2021-08-15_map.json",
   "2021-09-01_map.json",
+  "2021-09-15_map.json",
   "2021-10-01_map.json",
+  "2021-10-15_map.json",
   "2021-11-01_map.json",
+  "2021-11-15_map.json",
   "2021-12-01_map.json",
+  "2021-12-15_map.json",
   "2022-01-01_map.json",
+  "2022-01-15_map.json",
   "2022-02-01_map.json",
+  "2022-02-15_map.json",
   "2022-03-01_map.json",
+  "2022-03-15_map.json",
   "2022-04-01_map.json"
 ]
 
@@ -294,31 +322,32 @@ useEffect(() => {
         />
       }
       </div>
+      {/* <p className="statsOverview">Current Date: {currentIndex}</p> */}
       <p className="statsOverview">Total Cases: {totalCases} &emsp;&emsp; Population Vaccinated: {percentVaccinated}%</p>
-      <Box className="Slider" sx={{ width: 300 }}>
-        <Slider
-          aria-label="Time Selection Slider"
-          className="Slider"
-          sx = {{ color: "rgba(209, 13, 13, 0.8)"}}
-          getAriaValueText={valuetext}
-          defaultValue={dates.length-1}
-          marks
-          min={0}
-          max={dates.length - 1}
-          onChange={handleChange}
-          step={1}
-          value={currentIndex}
-          valueLabelFormat={valueLabelFormat}
-          valueLabelDisplay="auto"
-        />
-        
-      </Box>
-      <Box className="playButton">
-        <Button 
-        color="error"
-        onClick={handlePlay}>
-          PLAY
-        </Button>
+      <Box className="Slider" sx={{ width: 500 }}>
+        <Box className="SliderLeft">
+          <Slider
+            aria-label="Time Selection Slider"
+            className="Slider"
+            sx = {{ color: "rgba(209, 13, 13, 0.8)"}}
+            getAriaValueText={valuetext}
+            defaultValue={dates.length-1}
+            marks
+            min={0}
+            max={dates.length - 1}
+            onChange={handleChange}
+            step={1}
+            value={currentIndex}
+            valueLabelFormat={valueLabelFormat}
+            valueLabelDisplay="auto"
+          />
+        </Box>
+        <Box className="SliderRight">
+        {!sliderPlaying ?  
+          <FaPlay color="white" onClick={handlePlay}> </FaPlay> :
+          <FaPause color="white" onClick={handlePlay}> </FaPause>
+        }
+        </Box>
       </Box>
       <Box className="toggle">
         <FormControlLabel
